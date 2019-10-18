0751835537424bdcf36a152256cb0aa8f977d8bb
goreleaserbot <goreleaser@carlosbecker.com>
2019-10-18 17:57:20 +0530

    Brew formula update for harness-cli-binaries version v1.0.5

--- harness.rb
+++ harness.rb
@@ -2,16 +2,16 @@
 class Harness < Formula
   desc "Command Line Utility for Harness"
   homepage "https://app.harness.io/"
-  version "1.0.4"
+  version "1.0.5"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/wings-software/harness-cli-binaries/releases/download/v1.0.4/harnesscli_Darwin_x86_64.tar.gz"
-    sha256 "8c754715bc55e4a355baa7e38d7f67db4cedc208f0ec4323bbd4079741152d42"
+    url "https://github.com/wings-software/harness-cli-binaries/releases/download/v1.0.5/harnesscli_Darwin_x86_64.tar.gz"
+    sha256 "ea9c24415c232eb906c8cc43b427bc47cae3118494baf77f7002570d6d31ddfc"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/wings-software/harness-cli-binaries/releases/download/v1.0.4/harnesscli_Linux_x86_64.tar.gz"
-      sha256 "095a308071df34fe44df890050a20fcc4d5ce7f707264b3f8425a245115a3740"
+      url "https://github.com/wings-software/harness-cli-binaries/releases/download/v1.0.5/harnesscli_Linux_x86_64.tar.gz"
+      sha256 "692b59125fa5e0e45dffbd0f34bf3cda70242302660d07cb4847c2f1b79ea5fa"
     end
   end
 
